3fd9dfa903a86581fd9775a83bdf2476b7c7f6ab
Devine Lu Linvega <aliceffekt@gmail.com>
2023-07-23 20:04:11 -0700

    Fixed clear tile issue

--- build.sh
+++ build.sh
@@ -1,42 +1,35 @@
 #!/bin/sh -e
 
+ID="orca"
 ASM="uxncli $HOME/roms/drifblim.rom"
 EMU="uxnemu"
 LIN="uxncli $HOME/roms/uxnlin.rom"
-
-SRC="src/orca.tal"
-DST="bin/orca.rom"
-
+SRC="src/${ID}.tal"
+DST="bin/${ID}.rom"
 CPY="$HOME/roms"
 ARG="etc/tests.orca"
 
 STORE="$HOME/bin/butler push"
 STOREID="hundredrabbits/orca:uxn"
 
-echo ">> Cleaning"
 rm -rf bin
 mkdir bin
 
 if [[ "$*" == *"--lint"* ]]
 then
-    echo ">> Linting $SRC"
 	$LIN $SRC
 fi
 
-echo ">> Assembling $SRC"
 $ASM $SRC $DST
 
 if [[ "$*" == *"--save"* ]]
 then
-    echo ">> Saving $DST"
 	cp $DST $CPY
 fi
 
 if [[ "$*" == *"--push"* ]]
 then
-    echo ">> Pushing $DST"
 	$STORE $DST $STOREID
 fi
 
-echo ">> Running $DST"
 $EMU $DST $ARG
